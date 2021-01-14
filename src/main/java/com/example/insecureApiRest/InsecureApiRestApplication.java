2fdca3ce245758533fcd2f17fad7bd3a1cc534f2
Larissa Fonseca <larissafabiao@usp.br>
2021-01-14 20:43:15 -0300

    first commit

---
+++ src/main/java/com/example/insecureApiRest/InsecureApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.insecureApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsecureApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsecureApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/insecureApiRest/InsecureApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insecureApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsecureApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
